6b46de72a1b0210869e0345212dbf38c41339fae
yhkwak <154938948+yhkwak@users.noreply.github.com>
2024-11-19 09:22:46 +0900

    API 코드 환경변수로 불러오기로 수정

--- chatgpt_rag.py
+++ chatgpt_rag.py
@@ -1,10 +1,13 @@
 import os
+from dotenv import load_dotenv
 import pandas as pd
 from langchain_experimental.agents import create_pandas_dataframe_agent
 from langchain_community.chat_models import ChatOpenAI
 from langchain.agents.agent_types import AgentType
 
-os.environ['OPENAI_API_KEY'] = "sk-svcacct-Q77qk6FjT99qZpKYNEmm72Dr7N28CSt9QO0WYtfqgO3XyQJtFildaVBnYaQhs-uj0T3BlbkFJ2h191cVCfPlwajbvNHvkZkIpgUFm7Mgd8x8OmyctJaAJ7KyMB9kCHDaYN1rXxgBAA"
+load_dotenv()
+
+os.environ['OPENAI_API_KEY'] = os.getenv('OPENAI_API_KEY')
 
 # CSV 파일 경로
 csv_file_path = "data/medicine_update.csv"
@@ -30,15 +33,17 @@ agent = create_pandas_dataframe_agent(
 question = "열이 37도정도 되고 목이 아프고 콧물이 많이나요"
 prompt = f"""
 {question}
-라는 질문을 보고 증상을 파악하여, csv 파일의 효능 부분을 참고해
+라는 질문을 보고 증상을 파악하여, csv 파일을 랜덤하게 섞은 후, 효능 부분을 참고해
 가장 적절한 약품을 '제품명' 컬럼에서 서로 다르게 3가지 추천해주세요.
 답변은 반드시 한국어로 해주세요.
 제품은 반드시 csv 파일 안에 있는 제품으로 추천해주고, 제품명의 왜곡 없이 출력해주세요.
+csv 파일의 데이터 출처는 질병청 의약품안전나라 의약품통합정보시스템에서 가져왔다고 밝혀주세요.
 
 다음 형식으로 답변해주세요:
 1. [제품명] - [주요 효능]
 2. [제품명] - [주요 효능]
 3. [제품명] - [주요 효능]
+
 """
 
 # 에이전트 실행
